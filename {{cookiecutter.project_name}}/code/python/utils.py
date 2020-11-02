bdd892903cdd8a43dd51eba8178d2fa31108a100
ulgu3559 <ulgu3559@colorado.edu>
2020-11-02 09:50:49 -0700

    Add template

--- {{cookiecutter.project_name}}/code/python/utils.py
+++ {{cookiecutter.project_name}}/code/python/utils.py
@@ -7,18 +7,7 @@ test_data = pd.read_parquet(
     "/home/ulgu3559/oda-repos/yield-model/data/transformed/yield.parquet"
 )
 
-bubu = """
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
-   "http://www.w3.org/TR/html4/strict.dtd">
-<HTML>
-   <HEAD>
-      <TITLE>My first HTML document</TITLE>
-   </HEAD>
-   <BODY>
-      <DIV></DIV>
-   </BODY>
-</HTML>
-"""
+
 
 def eda_report_by_partition(df: pd.DataFrame, partition: str) -> pd.DataFrame:
     if partition in df.columns:
