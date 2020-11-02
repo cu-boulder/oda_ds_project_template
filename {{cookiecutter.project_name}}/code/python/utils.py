a9ebfffc83c841b9dff627ab3b1931ec5ef0c44f
ulgu3559 <ulgu3559@colorado.edu>
2020-11-02 14:25:00 -0700

    Add working implementation of report per partition

--- {{cookiecutter.project_name}}/code/python/utils.py
+++ {{cookiecutter.project_name}}/code/python/utils.py
@@ -1,24 +1,31 @@
 import pandas as pd
 from pandas_profiling import ProfileReport
 from bs4 import BeautifulSoup
-from jinja2 import Template
+from pathlib import Path
+from jinja2 import Environment, FileSystemLoader
 
 test_data = pd.read_parquet(
     "/home/ulgu3559/oda-repos/yield-model/data/transformed/yield.parquet"
 )
 
+template = Environment(
+    loader=FileSystemLoader(Path("../../code/python/templates").resolve())
+).get_template("report.html")
 
 
 def eda_report_by_partition(df: pd.DataFrame, partition: str) -> pd.DataFrame:
     if partition in df.columns:
         partitions = df.groupby(partition)
+        variables_section = []
         for group in partitions:
             partition_name = group[0]
-            _ = pd.DataFrame(group[1])
-            soup = BeautifulSoup(ProfileReport(_, minimal=True).to_html()).find_all(
-                "div", attrs={"class": "section-items"}
-            )
-            print(soup)
+            _ = pd.DataFrame(group[1]).reset_index()
+            section_items = BeautifulSoup(
+                ProfileReport(_, minimal=True).to_html(), features="html.parser"
+            ).find_all("div", attrs={"class": "section-items"})
+            variables_section.append(section_items)
+        with open("hello.html", "w") as fh:
+            fh.write(template.render(variables_section=variables_section))
 
 
 eda_report_by_partition(test_data, "YEAR")
