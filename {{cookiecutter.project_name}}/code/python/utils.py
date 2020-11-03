cda6254fb2242856e2b68efbef9eaff8d1c55291
ulgu3559 <ulgu3559@colorado.edu>
2020-11-03 10:18:03 -0700

    Add warnings section

--- {{cookiecutter.project_name}}/code/python/utils.py
+++ {{cookiecutter.project_name}}/code/python/utils.py
@@ -6,17 +6,18 @@ from jinja2 import Environment, FileSystemLoader
 
 test_data = pd.read_parquet(
     "/home/ulgu3559/oda-repos/yield-model/data/transformed/yield.parquet"
-).filter(
-    items=[
-        "YEAR",
-        "RESIDENCY_STATUS",
-        "ETHNICITY",
-        "ADDRESS_REGION",
-        "PERSON_FAMILY_SIZE",
-        "PERSON_HS_RANK_PERCENTILE",
-        "DEPOSIT_PAID",
-    ]
 )
+# ).filter(
+#     items=[
+#         "YEAR",
+#         "RESIDENCY_STATUS",
+#         "ETHNICITY",
+#         "ADDRESS_REGION",
+#         "PERSON_FAMILY_SIZE",
+#         "PERSON_HS_RANK_PERCENTILE",
+#         "DEPOSIT_PAID",
+#     ]
+# )
 
 template = Environment(
     loader=FileSystemLoader(Path("../../code/python/templates").resolve())
@@ -30,23 +31,15 @@ def eda_report_by_partition(df: pd.DataFrame, col_name: str) -> pd.DataFrame:
         for group in df.groupby(col_name):
             partition_name = group[0]
             _ = pd.DataFrame(group[1]).reset_index()
-            doc = BeautifulSoup(
+            dom = BeautifulSoup(
                 ProfileReport(_, minimal=True).to_html(), features="html.parser"
             )
             overview_section.setdefault(partition_name, []).extend(
-                doc.select("#overview-dataset_overview")
+                dom.select("#overview-dataset_overview")
             )
-            # overview_section.extend(doc.select("#overview-dataset_overview"))
-        # print(overview_section)
-        # # variables_section.extend(section_items)
-        # # print(section_items)
-        # print(section_items.select(".variable"))
-        # for element in section_items:
-        #     print(element)
-        # variable_name = element.find("p", attrs={"class": "h4"})["title"]
-        # if variable_name in variables_section:
-        #     variables_section[variable_name].append(element)
-        # print(variables_section.keys)
+            overview_section.setdefault(partition_name, []).extend(
+                dom.select("#overview-warnings")
+            )
         with open("hello.html", "w") as fh:
             fh.write(template.render(overview_section=overview_section))
 
