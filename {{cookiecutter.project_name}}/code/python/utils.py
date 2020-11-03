3139357e1ae3d3665ab901872a2480361b764ff5
ulgu3559 <ulgu3559@colorado.edu>
2020-11-03 15:00:50 -0700

    Add variables_section to function

--- {{cookiecutter.project_name}}/code/python/utils.py
+++ {{cookiecutter.project_name}}/code/python/utils.py
@@ -6,28 +6,27 @@ from jinja2 import Environment, FileSystemLoader
 
 test_data = pd.read_parquet(
     "/home/ulgu3559/oda-repos/yield-model/data/transformed/yield.parquet"
+).filter(
+    items=[
+        "YEAR",
+        "RESIDENCY_STATUS",
+        "ETHNICITY",
+        "ADDRESS_REGION",
+        "PERSON_FAMILY_SIZE",
+        "PERSON_HS_RANK_PERCENTILE",
+        "DEPOSIT_PAID",
+    ]
 )
-# ).filter(
-#     items=[
-#         "YEAR",
-#         "RESIDENCY_STATUS",
-#         "ETHNICITY",
-#         "ADDRESS_REGION",
-#         "PERSON_FAMILY_SIZE",
-#         "PERSON_HS_RANK_PERCENTILE",
-#         "DEPOSIT_PAID",
-#     ]
-# )
 
 template = Environment(
     loader=FileSystemLoader(Path("../../code/python/templates").resolve())
 ).get_template("report.html")
 
 
-def eda_report_by_partition(df: pd.DataFrame, col_name: str) -> pd.DataFrame:
+def eda_report_by_partition(df: pd.DataFrame, col_name: str):
     if col_name in df.columns:
         overview_section = {}
-        # variables_section = {}
+        variables_section = {}
         for group in df.groupby(col_name):
             partition_name = group[0]
             _ = pd.DataFrame(group[1]).reset_index()
@@ -40,8 +39,20 @@ def eda_report_by_partition(df: pd.DataFrame, col_name: str) -> pd.DataFrame:
             overview_section.setdefault(partition_name, []).extend(
                 dom.select("#overview-warnings")
             )
+            features = dom.select(".variable")
+            for feature in features:
+                # relies on the pandas profiling report structure
+                feature_name = feature.div.p["title"]
+                variables_section.setdefault(partition_name, {}).setdefault(
+                    feature_name, []
+                ).extend(feature)
         with open("hello.html", "w") as fh:
-            fh.write(template.render(overview_section=overview_section))
+            fh.write(
+                template.render(
+                    overview_section=overview_section,
+                    variables_section=variables_section,
+                )
+            )
 
 
 eda_report_by_partition(test_data, "YEAR")
