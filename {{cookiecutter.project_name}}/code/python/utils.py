8195cb02354092946af73adb71d2cbd699ffd9fa
ulgu3559 <ulgu3559@colorado.edu>
2020-11-04 10:05:53 -0700

    Refactor functions

--- {{cookiecutter.project_name}}/code/python/utils.py
+++ {{cookiecutter.project_name}}/code/python/utils.py
@@ -1,34 +1,25 @@
 import pandas as pd
 from pandas_profiling import ProfileReport
 from bs4 import BeautifulSoup
+from typing import Union
 from pathlib import Path
 from jinja2 import Environment, FileSystemLoader
 
-test_data = pd.read_parquet(
-    "/home/ulgu3559/oda-repos/yield-model/data/transformed/yield.parquet"
-)
-# .filter(
-#     items=[
-#         "YEAR",
-#         "RESIDENCY_STATUS",
-#         "ETHNICITY",
-#         "ADDRESS_REGION",
-#         "PERSON_FAMILY_SIZE",
-#         "PERSON_HS_RANK_PERCENTILE",
-#         "DEPOSIT_PAID",
-#     ]
-# )
-
-template = Environment(
-    loader=FileSystemLoader(Path("../../code/python/templates").resolve())
-).get_template("report.html")
 
 
-def eda_report_by_partition(df: pd.DataFrame, col_name: str):
-    if col_name in df.columns:
+def get_report_template(template: str):
+    return Environment(
+        loader=FileSystemLoader(Path("../../code/python/templates").resolve())
+    ).get_template(template)
+
+
+def eda_report_by_partition(
+    df: pd.DataFrame, partition: str, report_name: str = "eda_report_by_partition"
+):
+    if partition in df.columns:
         overview_section = {}
-        variables_section = {}
-        for group in df.groupby(col_name):
+        features_section = {}
+        for group in df.groupby(partition):
             partition_name = group[0]
             _ = pd.DataFrame(group[1]).reset_index()
             dom = BeautifulSoup(
@@ -42,18 +33,18 @@ def eda_report_by_partition(df: pd.DataFrame, col_name: str):
             )
             for feature in dom.select(".variable"):
                 feature_name = feature.div.p["title"]
-                variables_section.setdefault(feature_name, {}).setdefault(
+                features_section.setdefault(feature_name, {}).setdefault(
                     partition_name, []
                 ).append(feature)
-        with open("hello.html", "w") as fh:
-            fh.write(
+        # relies on the current project folder structure
+        out_path = Path("../../outputs/reports").resolve()
+        template = get_report_template("eda_by_partition.html")
+        with open(out_path / f"{report_name}.html", "w") as report:
+            report.write(
                 template.render(
                     overview_section=overview_section,
-                    variables_section=variables_section,
+                    features_section=features_section,
                 )
             )
     else:
-        raise KeyError(f"{col_name} is missing from columns")
-
-
-eda_report_by_partition(test_data, "YEAR")
+        raise KeyError(f"{partition} is missing from columns")
