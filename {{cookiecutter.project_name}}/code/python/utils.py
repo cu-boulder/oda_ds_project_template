54a1d88c7827784ffcc5295e0684d094f93e609c
ulgu3559 <ulgu3559@colorado.edu>
2020-11-04 11:21:21 -0700

    Add docstrings

--- {{cookiecutter.project_name}}/code/python/utils.py
+++ {{cookiecutter.project_name}}/code/python/utils.py
@@ -8,6 +8,12 @@ from jinja2 import Environment, FileSystemLoader
 
 
 def get_report_template(template: str):
+    """Gets a Jinja2 html template from the templates folder.
+
+    Args:
+        template (str): Jinja2 html template
+
+    """
     return Environment(
         loader=FileSystemLoader(Path("../../code/python/templates").resolve())
     ).get_template(template)
@@ -16,6 +22,18 @@ def get_report_template(template: str):
 def eda_report_by_partition(
     df: pd.DataFrame, partition: str, report_name: str = "eda_report_by_partition"
 ):
+    """Generate a profile report by partition from a pandas DataFrame.
+
+    Args:
+        df (pd.DataFrame)
+        partition (str): An existing column in the DataFrame which values should
+        be used as partitions.
+        report_name (str, optional): The name of the html report. 
+        Defaults to "eda_report_by_partition".
+
+    Raises:
+        KeyError: The partition name is not in the provided DataFrame
+    """
     if partition in df.columns:
         overview_section = {}
         features_section = {}
