b0c1af6f4310e05df4431eb8c85f424259292596
ulgu3559 <ulgu3559@colorado.edu>
2021-01-11 10:40:53 -0700

    Modify path for tests

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -6,7 +6,10 @@ logger = logging.getLogger(__name__)
 
 def remove_test_files():
     logger.info("Removing test files")
-    tests = Path("{{cookiecutter.project_name}}/code/python/tests")
+    tests = (
+        Path(__file__).resolve().parents[0]
+        / "{{cookiecutter.project_name}}/code/python/tests"
+    )
     for f in tests.iterdir():
         if f.is_file() and ".gitkeep" not in tests.stem:
             f.unlink()
