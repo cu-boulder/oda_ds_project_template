4277d3bdce1508f7084bfd7452b909a9e6d19b48
ulgu3559 <ulgu3559@colorado.edu>
2021-01-12 09:58:04 -0700

    Add hook for deleting great expectations suites

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -7,7 +7,7 @@ logger = logging.getLogger(__name__)
 
 
 def rm_non_gitkeep(folder: Literal["great_expectations", "tests"]):
-    path = Path("code/python" / folder)
+    path = Path(f"code/python/{folder}")
     for f in path.iterdir():
         if f.is_file() and f.stem != ".gitkeep":
             f.unlink()
